8fc1f2b154ae823fed6bb077e7ac0b88e2596fe1
vijay.pk061@gmail.com <vijay.pk061@gmail.com>
2025-08-10 20:13:29 +0530

    adding controller login

---
+++ Controllers/login.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolManagementV1.Controllers
+{
+    public class login : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
